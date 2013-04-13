e926218941005d87e84d210d5f0fb2e261deb5b0
Apprentice <apprentice@devbootcamp.com>
2013-04-13 14:43:40 -0700

    fixed seed file

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -9,6 +9,7 @@ class SchoolsController < ApplicationController
 	end
 
 	def search
+		# if params[:zip].blank? && params[:city].blank?
 		@schools = School.find_or_call(params[:zip], params[:state], params[:city]) 
 		render :json => @schools.to_json
 	end
