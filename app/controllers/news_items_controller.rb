63390a3b824f74f1cd1b8396f5c5c028e16c2c0f
Apprentice <apprentice@devbootcamp.com>
2013-04-12 20:00:30 -0700

    Popularity algorithm works with Tumblr posts. View refactor.

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,5 +1,5 @@
 class NewsItemsController < ApplicationController
-
+  include ApplicationHelper
 
   def index
 
@@ -7,7 +7,9 @@ class NewsItemsController < ApplicationController
     # @tweets = Twitter.search('#edchat OR #education', :count => 10, :result_type => 'popular').statuses
 
     client = Tumblr::Client.new
-    @tumbles = client.tagged('education', :limit => 10)
+    search = client.tagged('education', :limit => 5)
+
+    @tumbles = add_popularity(search)
 
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def add_popularity(posts)
+
+    posts.map do |post|
+      hours = ((Time.now - post['date'].to_time) / 60) / 60
+
+      popularity = (post['note_count'].to_i - 1) / ((hours + 2) ** 1.5)
+
+      post.merge!({'popularity' => (popularity * 10).ceil })
+    end
+    
+  end
+  
+
 end
