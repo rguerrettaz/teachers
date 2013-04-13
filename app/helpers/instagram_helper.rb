a86800b66f0581f1e691d8988084ccac2802d700
Apprentice <apprentice@devbootcamp.com>
2013-04-13 15:11:50 -0700

    merge changes with instagram helper

--- app/helpers/instagram_helper.rb
+++ app/helpers/instagram_helper.rb
@@ -9,7 +9,6 @@ module InstagramHelper
     posts.flatten!
   end
 
-
   def from_insta
     insta_search.map do |pic|
       NewsItem.new(:published_at => pic.created_time,
@@ -29,22 +28,46 @@ module InstagramHelper
 
 
   def find_by_coords(lat, lng, school_name)
+    @id = nil
     locations = Instagram.location_search(lat, lng)
 
     locations.each do |loc|
       distance = (Levenshtein.normalized_distance(loc.name, school_name) * 100)
       unless distance < 25
-        puts "Nope!"
+        puts "Nope! It's #{loc.id}, #{loc.name}!"
       else
         puts "Yep! It's #{loc.id}, #{loc.name}!"
         @id = loc.id
         images_by_loc(@id)
       end
     end
+
+    puts "\n***** @id is #{@id}"
+
+    if @id == nil
+      puts "***** LOCATION NOT FOUND *****"
+      puts "*****  TIME FOR PLAN B   *****"
+      images_by_coords(lat, lng)
+    end
   end
 
   def images_by_loc(loc)
-    @location_images = Instagram.location_recent_media(loc)
+    @location_images = Instagram.location_recent_media(loc, :count => 10)
+    puts "***** #{@location_images.length} images found by location"
+  end
+
+  def images_by_coords(lat, lng)
+    @location_images = Instagram.media_search(lat, lng, :count => 10, :distance => 100)
+    puts "***** #{@location_images.length} images found by coordinates"
+  end
+
+  def image_url(object)
+    @url             = object.images.standard_resolution.url
+    unless object.caption.nil?
+      @profile_picture = object.caption.from.profile_picture
+      @username        = object.caption.from.username
+      @caption         = object.caption.text.html_safe
+    end
   end
 
 
