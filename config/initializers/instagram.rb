204eae49c8cb4a4f0d562ea6c0acfaac69b11273
Gavin St. Ours <charmcitygavin@gmail.com>
2013-04-15 12:14:51 -0700

    Change callback url to local for local testing.

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,7 @@
-CALLBACK_URL = "http://grit-test.herokuapp.com/oauth/callback"
+CALLBACK_URL = "http://127.0.0.1:3000/oauth/callback"
 
 Instagram.configure do |config|
   config.client_id = "97bf686e3d954eb4b239dea374b9147a"
   config.client_secret = "0cde61ab695848e3a37ba19843fd310e"
 end
+
