8884f9a953fea041e032c878743e5fb664a783b5
Apprentice <apprentice@devbootcamp.com>
2013-04-16 20:02:11 -0700

    remove old migrations

--- db/migrate/20130416203049_string_to_text.rb
+++
@@ -1,9 +0,0 @@
-class StringToText < ActiveRecord::Migration
-  def change
-    change_table :news_items do |t|
-      t.text :caption
-      t.text :question
-      t.text :answer
-    end
-  end
-end
