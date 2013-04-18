239ebeb46b6c3e0962d7f1d69a107ca34c78ebf4
Apprentice <apprentice@devbootcamp.com>
2013-04-18 13:37:36 -0700

    merge

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -18,5 +18,19 @@ var Search = {
 
 $(document).ready(function() {
 	Search.init();
+
+	$('.nav-pills').on('click', 'a', function(e) {	
+		e.preventDefault();
+		$('.nav-pills li').removeClass('active');
+		$(this).parent().addClass('active');
+		var link_name = $(this).text();
+		var filter_name = "." + link_name.toLowerCase();
+		if (filter_name == '.all') {
+			$('#news_item_grid').isotope({ filter: '.news_item_wrapper' });
+		}
+		else {   
+		$('#news_item_grid').isotope({ filter: filter_name });
+		}
+	});
 });
 
